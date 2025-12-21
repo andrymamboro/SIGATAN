45fb947d0d08b43ba4357b75387f935c39802336
Desya Putri Anandari <andry_saringgan85@gmail.com>
2025-12-21 19:06:30 +0800

    Updates

---
+++ assets/db-CzWppX9X.js
@@ -0,0 +1,2 @@
+import{c as s}from"./vendor-CDA6I53B.js";const t="https://wfctxoulksmervtxghwr.supabase.co",a="sb_publishable_P-1TRDahxuSxl8DFcne2cA_b6NHo2Ow",e=s(t,a);export{e as s};
+//# sourceMappingURL=db-CzWppX9X.js.map
