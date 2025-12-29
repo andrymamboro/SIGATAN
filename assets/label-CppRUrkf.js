a5fdc74388e6f701d0a3e2ea87d7de44e2ed9714
Desya Putri Anandari <andry_saringgan85@gmail.com>
2025-12-30 00:51:04 +0800

    Updates

--- assets/label-CppRUrkf.js
+++
@@ -1,2 +0,0 @@
-import{r as a,j as e,aT as s,e as o}from"./vendor-k34wZweL.js";import{a as r}from"./index-DXIltddd.js";const d=o("text-sm font-medium   leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"),m=a.forwardRef(({className:a,...o},m)=>e.jsx(s,{ref:m,className:r(d(),a),...o}));m.displayName=s.displayName;export{m as L};
-//# sourceMappingURL=label-CppRUrkf.js.map
