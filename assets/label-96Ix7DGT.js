f87121c683bc98431066cf55fb4640cdf396118a
Desya Putri Anandari <andry_saringgan85@gmail.com>
2026-01-01 14:40:47 +0800

    Updates

---
+++ assets/label-96Ix7DGT.js
@@ -0,0 +1,2 @@
+import{r as a,j as e,aU as s,e as o}from"./vendor-B-UrjPD3.js";import{a as r}from"./index-DFbN_dxx.js";const d=o("text-sm font-medium   leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"),m=a.forwardRef(({className:a,...o},m)=>e.jsx(s,{ref:m,className:r(d(),a),...o}));m.displayName=s.displayName;export{m as L};
+//# sourceMappingURL=label-96Ix7DGT.js.map
